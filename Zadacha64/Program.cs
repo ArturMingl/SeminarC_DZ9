3ae564bdfa0cb64cfc5e8ee5d0a83afac914cd05
ArturMingl <adiscov90@gmail.com>
2022-12-24 17:18:39 +0300

    Complete

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+void NumberCounter(int number)
+{
+    if (number < 0)
+        Console.WriteLine($"{number} не натуральное число");
+    if (number == 0)
+        return;
+    Console.Write($"{number}, ");
+    NumberCounter(number - 1);
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NumberCounter(number);
---
+++ Zadacha66/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N
+
+
+int SumNumber(int numberM, int numberN)
+{
+    if (numberM == numberN) return 0;
+    if (numberM < numberN) return numberN + SumNumber(numberM, numberN - 1);
+    else return numberN + SumNumber(numberM, numberN + 1);
+}
+
+Console.WriteLine("Введите первое число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Сумма чисел от {numberM} до {numberN} равна {SumNumber(numberM, numberN)}");
